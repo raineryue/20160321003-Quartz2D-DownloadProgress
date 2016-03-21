0fb92466519f58c6748bbef98efff0d78041569b
raineryue <it_jinlei.yue@163.com>
2016-03-21 22:41:53 +0800

    Initial Commit

---
+++ 20160321003-Quartz2D-DownloadProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160321003-Quartz2D-DownloadProgress
+//
+//  Created by Rainer on 16/3/21.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160321003-Quartz2D-DownloadProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160321003-Quartz2D-DownloadProgress
+//
+//  Created by Rainer on 16/3/21.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
